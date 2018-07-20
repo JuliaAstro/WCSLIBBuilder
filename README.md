63d6f2b3c0b1d4282dcfaf93c7c9d0450114ef14
giordano <mose@gnu.org>
2018-07-20 21:38:39 +0200

    Add autogenerated files to build WCS

---
+++ README.md
@@ -0,0 +1,9 @@
+# WCSLIBBuilder
+
+[![Build Status](https://travis-ci.org/giordano/WCSLIBBuilder.svg?branch=master)](https://travis-ci.org/giordano/WCSLIBBuilder)
+
+This repository builds binary artifacts for the WCS project. Binary artifacts are automatically uploaded to
+[this repository's GitHub releases page](https://github.com/giordano/WCSLIBBuilder/releases) whenever a tag is created
+on this repository.
+
+This repository was created using [BinaryBuilder.jl](https://github.com/JuliaPackaging/BinaryBuilder.jl)
---
+++ build_tarballs.jl
@@ -0,0 +1,49 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+# Collection of sources required to build WCS
+sources = [
+    "https://cache.julialang.org/ftp://ftp.atnf.csiro.au/pub/software/wcslib/wcslib-5.13.tar.bz2" =>
+    "d6983e8bc5997e625e66cc3b8590745231f1761437d533ad4b99a015eeb9b4e7",
+
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd wcslib-5.13/
+./configure --prefix=$prefix --host=$target --disable-fortran --without-cfitsio --without-pgplot --disable-utils
+make
+make install
+
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    Linux(:i686, :glibc),
+    Linux(:x86_64, :glibc),
+    Linux(:aarch64, :glibc),
+    Linux(:armv7l, :glibc, :eabihf),
+    Linux(:powerpc64le, :glibc),
+    Linux(:i686, :musl),
+    Linux(:x86_64, :musl),
+    Linux(:aarch64, :musl),
+    Linux(:armv7l, :musl, :eabihf),
+    FreeBSD(:x86_64)
+]
+
+# The products that we will ensure are always built
+products(prefix) = [
+    LibraryProduct(prefix, "libwcs", :libwcs)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, "WCS", sources, script, platforms, products, dependencies)
+
