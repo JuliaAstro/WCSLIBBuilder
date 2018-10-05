4f5459ba8c3c241c414ba6ae41126ac438a22935
Mosè Giordano <mose@gnu.org>
2018-10-06 01:20:01 +0200

    Update to new release of BinaryBuilder

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -2,9 +2,10 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder
 
-# Collection of sources required to build WCS
 name = "WCS"
-version = v"5.20"
+version = v"5.20.0"
+
+# Collection of sources required to build WCS
 sources = [
     "https://cache.julialang.org/ftp://ftp.atnf.csiro.au/pub/software/wcslib/wcslib-5.20.tar.bz2" =>
     "0bc037db498cbd8e17ecaedd72fad15d6810ae885793882d5736f746f64c0fb1",
@@ -35,15 +36,15 @@ fi
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = [
-    Linux(:i686, :glibc),
-    Linux(:x86_64, :glibc),
-    Linux(:aarch64, :glibc),
-    Linux(:armv7l, :glibc, :eabihf),
-    Linux(:powerpc64le, :glibc),
-    Linux(:i686, :musl),
-    Linux(:x86_64, :musl),
-    Linux(:aarch64, :musl),
-    Linux(:armv7l, :musl, :eabihf),
+    Linux(:i686, libc=:glibc),
+    Linux(:x86_64, libc=:glibc),
+    Linux(:aarch64, libc=:glibc),
+    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
+    Linux(:powerpc64le, libc=:glibc),
+    Linux(:i686, libc=:musl),
+    Linux(:x86_64, libc=:musl),
+    Linux(:aarch64, libc=:musl),
+    Linux(:armv7l, libc=:musl, call_abi=:eabihf),
     MacOS(:x86_64),
     FreeBSD(:x86_64),
     Windows(:i686),
@@ -62,4 +63,3 @@ dependencies = [
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
-
